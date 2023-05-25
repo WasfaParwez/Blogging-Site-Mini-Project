bf9f57bf224a7f5a91f2825acbc2fc84326a0499
prasadshivendra54 <tshivendra07@gmail.com>
2023-05-25 17:21:34 +0530

    i maked post apis in this project

--- index.js
+++ index.js
@@ -8,11 +8,14 @@ app.use(express.urlencoded({ extended:true }));
 app.use('/',route);
 
 
-mongoose.connect("mongodb+srv://parwezwasfa:FrEKcgz9j1UIuir3@cluster0.r5dosiq.mongodb.net/test",{
+// i added my MongoDB 
+mongoose.connect("mongodb+srv://tshivendra07:6sWDbb2xoYJ5IZ0N@cluster0.3dhywqg.mongodb.net/project-1-with-team?retryWrites=true&w=majority",{
     useNewUrlParser:true
 })
 .then(()=>console.log("you are now connected to MongoDB"))
 .catch(err=> console.log(err))
+
+
 
 app.listen(process.env.PORT||3000,function(){
     console.log('the app is running at port '+(process.env.PORT||3000));
--- route/route.js
+++ route/route.js
@@ -1,16 +1,13 @@
 const express = require('express');
 const route = express.Router();
-const authorcontroller= require("../controller/authorcontroller")
-
-route.get("/myfirstapi",function(req, res){
-    res.send("This is working")
-})
+const controller = require('../controller2/controller')
 
 
-route.post("/createauthor",authorcontroller.createauthor)
+//  this post api is for create author
+route.post("/createauthor", controller.createauthor)
 
+// this post api is for create blog
+route.post('/createBlog', controller.createBlog)
 
-
-
-
+// export route
 module.exports = route;
